77d50f2db0b0bf1b08b0648c8a47455afaffb763
fotovisp <fotovis@gmail.com>
2025-11-11 22:11:21 +0200

    pydantic2

--- pydantic_2.py
+++ pydantic_2.py
@@ -1,1 +1,15 @@
-print(111)
+from pydantic import BaseModel
+from typing import List
+
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+person = Person(name="John", age=42)
+js = person.model_dump_json(indent=4)
+print(js)
+
+with open("person.json", "w") as f:
+    f.write(js)
+
